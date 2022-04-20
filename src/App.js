f9546e2759288e52d50d6e7434267f6df91021a0
Edgar-Carrazco <edgar.carrazco@charter.net>
2022-04-19 19:36:26 -0500

    AWS Update

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import { SocialBCollection, NavBar, SideBar } from './ui-components';
+import { Flex } from '@aws-amplify/ui-react';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    <Flex direction="row">
+      <SideBar />
+      <SocialBCollection />
+    </Flex>;
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ui-components/Newsfeedexample.jsx
@@ -0,0 +1,210 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import React from "react";
+import { getOverrideProps } from "@aws-amplify/ui-react/internal";
+import { Flex, View } from "@aws-amplify/ui-react";
+export default function Newsfeedexample(props) {
+  const { overrides, ...rest } = props;
+  return (
+    <View
+      width="1440px"
+      height="2400px"
+      overflow="hidden"
+      position="relative"
+      padding="0px 0px 0px 0px"
+      backgroundColor="rgba(255,255,255,1)"
+      {...rest}
+      {...getOverrideProps(overrides, "Newsfeedexample")}
+    >
+      <Flex
+        gap="16px"
+        position="absolute"
+        top="-142px"
+        left="337px"
+        direction="column"
+        padding="0px 0px 0px 0px"
+        {...getOverrideProps(overrides, "Frame 1")}
+      >
+        <Flex
+          gap="0"
+          direction="column"
+          width="766px"
+          justifyContent="center"
+          shrink="0"
+          position="relative"
+          borderRadius="16px 16px 16px 16px"
+          padding="16px 16px 16px 16px"
+          backgroundColor="rgba(255,255,255,1)"
+          {...getOverrideProps(overrides, "Card18352345")}
+        ></Flex>
+        <Flex
+          gap="0"
+          direction="column"
+          width="766px"
+          justifyContent="center"
+          shrink="0"
+          position="relative"
+          borderRadius="16px 16px 16px 16px"
+          padding="16px 16px 16px 16px"
+          backgroundColor="rgba(255,255,255,1)"
+          {...getOverrideProps(overrides, "Card18352303")}
+        ></Flex>
+        <Flex
+          gap="0"
+          direction="column"
+          width="766px"
+          justifyContent="center"
+          shrink="0"
+          position="relative"
+          borderRadius="16px 16px 16px 16px"
+          padding="16px 16px 16px 16px"
+          backgroundColor="rgba(255,255,255,1)"
+          {...getOverrideProps(overrides, "Card18352388")}
+        ></Flex>
+        <Flex
+          gap="0"
+          direction="column"
+          width="766px"
+          justifyContent="center"
+          shrink="0"
+          position="relative"
+          borderRadius="16px 16px 16px 16px"
+          padding="16px 16px 16px 16px"
+          backgroundColor="rgba(255,255,255,1)"
+          {...getOverrideProps(overrides, "Card18352472")}
+        ></Flex>
+        <Flex
+          gap="0"
+          direction="column"
+          width="766px"
+          justifyContent="center"
+          shrink="0"
+          position="relative"
+          borderRadius="16px 16px 16px 16px"
+          padding="16px 16px 16px 16px"
+          backgroundColor="rgba(255,255,255,1)"
+          {...getOverrideProps(overrides, "Card18352473")}
+        ></Flex>
+        <Flex
+          gap="0"
+          direction="column"
+          width="766px"
+          justifyContent="center"
+          shrink="0"
+          position="relative"
+          borderRadius="16px 16px 16px 16px"
+          padding="16px 16px 16px 16px"
+          backgroundColor="rgba(255,255,255,1)"
+          {...getOverrideProps(overrides, "Card18352640")}
+        ></Flex>
+        <Flex
+          gap="0"
+          direction="column"
+          width="766px"
+          justifyContent="center"
+          shrink="0"
+          position="relative"
+          borderRadius="16px 16px 16px 16px"
+          padding="16px 16px 16px 16px"
+          backgroundColor="rgba(255,255,255,1)"
+          {...getOverrideProps(overrides, "Card18352641")}
+        ></Flex>
+        <Flex
+          gap="0"
+          direction="column"
+          width="766px"
+          justifyContent="center"
+          shrink="0"
+          position="relative"
+          borderRadius="16px 16px 16px 16px"
+          padding="16px 16px 16px 16px"
+          backgroundColor="rgba(255,255,255,1)"
+          {...getOverrideProps(overrides, "Card18352642")}
+        ></Flex>
+        <Flex
+          gap="0"
+          direction="column"
+          width="766px"
+          justifyContent="center"
+          shrink="0"
+          position="relative"
+          borderRadius="16px 16px 16px 16px"
+          padding="16px 16px 16px 16px"
+          backgroundColor="rgba(255,255,255,1)"
+          {...getOverrideProps(overrides, "Card18352643")}
+        ></Flex>
+        <Flex
+          gap="0"
+          direction="column"
+          width="766px"
+          justifyContent="center"
+          shrink="0"
+          position="relative"
+          borderRadius="16px 16px 16px 16px"
+          padding="16px 16px 16px 16px"
+          backgroundColor="rgba(255,255,255,1)"
+          {...getOverrideProps(overrides, "Card18352474")}
+        ></Flex>
+        <Flex
+          gap="0"
+          direction="column"
+          width="766px"
+          justifyContent="center"
+          shrink="0"
+          position="relative"
+          borderRadius="16px 16px 16px 16px"
+          padding="16px 16px 16px 16px"
+          backgroundColor="rgba(255,255,255,1)"
+          {...getOverrideProps(overrides, "Card18352475")}
+        ></Flex>
+        <Flex
+          gap="0"
+          direction="column"
+          width="766px"
+          justifyContent="center"
+          shrink="0"
+          position="relative"
+          borderRadius="16px 16px 16px 16px"
+          padding="16px 16px 16px 16px"
+          backgroundColor="rgba(255,255,255,1)"
+          {...getOverrideProps(overrides, "Card18352389")}
+        ></Flex>
+      </Flex>
+      <Flex
+        gap="568px"
+        position="absolute"
+        top="0%"
+        bottom="96.33%"
+        left="0%"
+        right="0%"
+        direction="row"
+        width="1440px"
+        justifyContent="center"
+        alignItems="center"
+        padding="24px 32px 24px 32px"
+        backgroundColor="rgba(255,255,255,1)"
+        {...getOverrideProps(overrides, "NavBar/Variant2")}
+      ></Flex>
+      <Flex
+        gap="10px"
+        position="absolute"
+        top="3.67%"
+        bottom="0%"
+        left="0%"
+        right="78.26%"
+        direction="row"
+        width="313px"
+        height="2312px"
+        alignItems="flex-start"
+        padding="32px 0px 32px 0px"
+        backgroundColor="rgba(255,255,255,1)"
+        {...getOverrideProps(overrides, "SideBar")}
+      ></Flex>
+    </View>
+  );
+}
